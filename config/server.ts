ec9c241af12a0d8b48cfef1b2703dc96ff0df6d6
Tamar Ron Marvin <trmarvin@gmail.com>
2025-10-12 00:41:57 +0300

    Fixing server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: '0.0.0.0',
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+  url: env('PUBLIC_URL', 'https://cms.trmarvin.org'), // must be https and match your domain
+  proxy: true, // critical: tells Koa to trust X-Forwarded-* headers
+  app: { keys: env.array('APP_KEYS') },
 });
