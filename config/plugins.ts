4f635061c096990e445be6be8fb767fc4ef32f18
Tamar Marvin <trmarvin@gmail.com>
2025-10-15 11:05:24 +0300

    Adding JWT_SECRET

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,8 @@
-export default () => ({});
+export default ({ env }) => ({
+  'users-permissions': {
+    config: {
+      // Reads JWT from env; you can use either name
+      jwtSecret: env('JWT_SECRET', env('USERS_PERMISSIONS_JWT_SECRET')),
+    },
+  },
+});
