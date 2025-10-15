d25d09319e29b03f0462945e0f7ddd1f0c24a2bb
Tamar Marvin <trmarvin@gmail.com>
2025-10-15 09:49:12 +0300

    Setting up local https env

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,12 @@
+import fs from 'fs';
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  url: env('PUBLIC_URL', 'https://localhost:1337'),
+  app: { keys: env.array('APP_KEYS') },
+  https: {
+    key: fs.readFileSync(env('SSL_KEY_PATH'), 'utf8'),
+    cert: fs.readFileSync(env('SSL_CERT_PATH'), 'utf8'),
   },
 });
