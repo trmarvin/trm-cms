418f853d03ec6914d2e26b5439da8e19e12563d6
Tamar Marvin <trmarvin@gmail.com>
2025-10-15 11:28:11 +0300

    internal server error fix

--- config/server.ts
+++ config/server.ts
@@ -2,5 +2,11 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL'),
+  proxy: true,
   app: { keys: env.array('APP_KEYS') },
+  // Optional: silence the deprecation later by migrating to session settings
+  // admin: {
+  //   auth: { secret: env('ADMIN_JWT_SECRET') },
+  // },
 });
